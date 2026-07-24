5307bcf80a9d7b63315df1a028b349ceafbdbbbd
Artem Shumeiko <shumeykoartem@mail.ru>
2026-07-24 17:16:44 +0300

    week 4: add celery routes, retry, acks late

--- samokat/src/samokat/infrastructure/tasks/celery_app.py
+++ samokat/src/samokat/infrastructure/tasks/celery_app.py
@@ -8,10 +8,16 @@ celery_app = Celery(
 )
 
 celery_app.conf.update(
+    worker_prefetch_multiplier=1,
+    task_acks_late=True,
     beat_schedule={
         "sync_darkstore_products_and_prices-every-5-minutes": {
             "task": "sync_darkstore_products_and_prices",
             "schedule": 60 * 5,
         },
+    },
+    task_routes={
+        "generate_order_report": {"queue": "critical"},
+        "sync_darkstore_products_and_prices": {"queue": "basic"},
     },
 )
--- samokat/src/samokat/infrastructure/tasks/celery_tasks.py
+++ samokat/src/samokat/infrastructure/tasks/celery_tasks.py
@@ -25,14 +25,24 @@ def sync_darkstore_products_and_prices() -> None:
     asyncio.run(_helper())
 
 
-@celery_app.task(name="generate_order_report")
+@celery_app.task(
+    name="generate_order_report",
+    autoretry_for=(Exception,),
+    retry_backoff=True,
+    max_retries=2,
+    acks_late=False,
+    # bind=True,
+)
 def generate_order_report(report_id: str) -> None:
     async def _helper() -> None:
         container = create_container(settings)
         logger.info("Report started")
         async with container(scope=Scope.REQUEST) as request_container:
             service = await request_container.get(ReportService)
+            # try:
             await service.generate_orders_report(report_id)
+            # except ValueError as exc:
+            #     raise self.retry(countdown=2, exc=exc)
         logger.info("Report finished")
 
     asyncio.run(_helper())
